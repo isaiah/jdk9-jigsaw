6140d4db3c2642894f70231b9197dcf0a7606f00
Liqun Peng <liqun.peng@springernature.com>
2017-07-04 16:26:36 +0200

    remove duplicated --patch-module example

--- session-1-jigsaw-intro/07_patch_module_option/src/com.greetings/com/greetings/Main.java
+++ session-1-jigsaw-intro/07_patch_module_option/src/com.greetings/com/greetings/Main.java
@@ -5,10 +5,11 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class Main {
     public static void main(String[] args) {
-        Map<Integer, String> myMap = new ConcurrentHashMap<>();
-        myMap.put(1, "one");
-        myMap.put(2, "two");
-        myMap.put(3, "three");
+        Map<String, Integer> myMap = new ConcurrentHashMap<>();
+        myMap.put("one", 1);
+        myMap.put("two", 2);
+        myMap.put("three", 3);
         System.out.println("Hello " + myMap);
+        System.out.println("Is Duke home? " + myMap.containsKey("Duke"));
     }
 }
